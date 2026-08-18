0c055ee48b904ae83ee25ad5e389f7470e32d507
Ritika Devi <ritikabandral73@gmail.com>
2026-08-18 13:46:02 -0700

    here again

--- src/loop.java
+++ src/loop.java
@@ -2,4 +2,8 @@ void main (){
     for(int i = 0 ; i < 5 ; i ++){
         System.out.println("Hello ritika , this is my first try to push my code to github");
     }
+    //successfully running
+    for(int j = 0 ; j < 6 ; j++){
+        System.out.println("Hello i am here again");
+    }
 }
