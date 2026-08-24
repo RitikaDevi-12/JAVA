ea4a5193036e3eb65f7611767f34af278772d104
Ritika Devi <ritikabandral73@gmail.com>
2026-08-23 21:38:05 -0700

    basic_oops

---
+++ src/basic_oops.java
@@ -0,0 +1,51 @@
+public class basic_oops{
+    public static void main(String[] args) {
+        // default constructo
+     class Student{
+        int id;
+        String name;
+        int age;
+        int  nos;
+     }
+         Student A = new Student();
+         A.id = 1;
+         A.name = "John";
+         A.age = 20;
+         A.nos = 101;
+
+         System.out.println(A.name);
+         System.out.println(A.id);
+         System.out.println(A.age);
+         System.out.println(A.nos);
+
+
+    }
+         
+        //  public Student()  {
+        //     System.out.println("Student ddefault constructor");
+        // }         
+ 
+
+        // // methods /  behaviour
+        // public void study(){
+        //     System.out.println(name + " is studying");
+        // }
+        // public void play(){
+        //     System.out.println(name + "is playing");
+        // }
+
+        // public void sleep(){
+        //     System.out.println(name + "is sleeping");
+
+        // }
+
+        // public void bunk(){
+        //     System.out.println(name + "is bunking";)
+        // }
+
+
+
+        }
+    
+
+
